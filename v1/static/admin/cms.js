7e8cfe31ed816cc4a614912b1d4f092ae4caa05e
Mohamed Nasser <mohamednasser2001@hotmail.com>
2023-08-11 18:45:04 +0300

    add identity widget

--- v1/static/admin/cms.js
+++ v1/static/admin/cms.js
@@ -1,8 +1,5 @@
 
 import AboutPreview from "./templates/about.js";
 
-CMS.registerPreviewStyle("/css/resume.css");
-CMS.registerPreviewStyle("/css/tweaks.css");
-
 CMS.registerPreviewTemplate("about", AboutPreview);
 CMS.init();
