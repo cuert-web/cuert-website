01e7c4a261ff0fb773358fabc92f1876899e3b54
abdallah farag <abdallah_altalbawy@yahoo.com>
2025-08-31 23:34:29 +0300

    fix: color change

--- v1/themes/hugo-universal-theme/static/js/gmaps.init.js
+++ v1/themes/hugo-universal-theme/static/js/gmaps.init.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   map()
 })
 
-function map () {
+function map() {
   if ($('#map').length) {
     var lat = $('#gmap-lat').val()
     var lng = $('#gmap-lng').val()
@@ -14,23 +14,23 @@ function map () {
     var styles =
       [
         {
-          'featureType': 'landscape', 'stylers': [{'saturation': -100}, {'lightness': 65}, {'visibility': 'on'}]
+          'featureType': 'landscape', 'stylers': [{ 'saturation': -100 }, { 'lightness': 65 }, { 'visibility': 'on' }]
         }, {
-          'featureType': 'poi', 'stylers': [{'saturation': -100}, {'lightness': 51}, {'visibility': 'simplified'}]
+          'featureType': 'poi', 'stylers': [{ 'saturation': -100 }, { 'lightness': 51 }, { 'visibility': 'simplified' }]
         }, {
-          'featureType': 'road.highway', 'stylers': [{'saturation': -100}, {'visibility': 'simplified'}]
+          'featureType': 'road.highway', 'stylers': [{ 'saturation': -100 }, { 'visibility': 'simplified' }]
         }, {
-          'featureType': 'road.arterial', 'stylers': [{'saturation': -100}, {'lightness': 30}, {'visibility': 'on'}]
+          'featureType': 'road.arterial', 'stylers': [{ 'saturation': -100 }, { 'lightness': 30 }, { 'visibility': 'on' }]
         }, {
-          'featureType': 'road.local', 'stylers': [{'saturation': -100}, {'lightness': 40}, {'visibility': 'on'}]
+          'featureType': 'road.local', 'stylers': [{ 'saturation': -100 }, { 'lightness': 40 }, { 'visibility': 'on' }]
         }, {
-          'featureType': 'transit', 'stylers': [{'saturation': -100}, {'visibility': 'simplified'}]
+          'featureType': 'transit', 'stylers': [{ 'saturation': -100 }, { 'visibility': 'simplified' }]
         }, {
-          'featureType': 'administrative.province', 'stylers': [{'visibility': 'off'}]
+          'featureType': 'administrative.province', 'stylers': [{ 'visibility': 'off' }]
         }, {
-          'featureType': 'water', 'elementType': 'labels', 'stylers': [{'visibility': 'on'}, {'lightness': -25}, {'saturation': -100}]
+          'featureType': 'water', 'elementType': 'labels', 'stylers': [{ 'visibility': 'on' }, { 'lightness': -25 }, { 'saturation': -100 }]
         }, {
-          'featureType': 'water', 'elementType': 'geometry', 'stylers': [{'hue': '#ffff00'}, {'lightness': -25}, {'saturation': -97}]
+          'featureType': 'water', 'elementType': 'geometry', 'stylers': [{ 'hue': '#ffff00' }, { 'lightness': -25 }, { 'saturation': -97 }]
         }
       ]
 
